868efa065f46d8570a9f38706be21bfa082fc026
YEJI <yedoong919@gmail.com>
2023-01-18 20:06:55 +0900

    Java의 정석 예제 3_6 (나머지 연산자 사용하기)

---
+++ src/exPrac/Excercise3_6.java
@@ -0,0 +1,16 @@
+package exPrac;
+
+public class Excercise3_6 {
+    public static void main(String[] args) {
+        /*아래는 변수 num의 값보다 크면서도 가장 가까운 10의 배수에서 변수 num의 값을 뺀 나머지를 구하는 코드이다.
+         예를 들어, 24의 크면서도 가장 가까운 10의 배수는 30이다.
+         19의 경우 20, 81의 경우 90이다.
+         30에서 24를 뺀 나머지는 6이기 때문에 변수 num의 값이 24라면 6을 결과로 얻어야 한다.
+
+         [Hint] 나머지 연산자를 사용하라*/
+
+        int num = 24;
+        //System.out.println( /* (1) */ );
+        System.out.println();
+    }
+}
